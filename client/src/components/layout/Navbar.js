b52af1ad53e44f5dea1b2c6d39141c66d0c632db
Arda Celik <arda.celik@uoit.net>
2019-07-03 21:52:43 -0400

    Created Navbar, added Bootstrap 4 and React Router

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div>
+      <nav
+        className="navbar navbar-expand-lg navbar-light"
+        style={{ backgroundColor: "#e3f2fd" }}
+      >
+        <Link to="/" className="navbar-brand">
+          <i className={icon} /> {title}
+        </Link>
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item active">
+            <Link to="/about" className="nav-link">
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string
+};
+
+Navbar.defaultProps = {
+  title: "Quote Keeper",
+  icon: "fa fa-quote-right"
+};
+
+export default Navbar;
