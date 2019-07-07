cd1bc97d0620a10af603d31b2bb402c2a73d2cc4
Arda Celik <arda.celik@uoit.net>
2019-07-07 19:28:40 -0400

    Auth context and initial state

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,28 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
-import "./App.css";
 
 import QuoteState from "./context/quote/QuoteState";
+import AuthState from "./context/auth/AuthState";
+import "./App.css";
 
 const App = () => {
   return (
-    <QuoteState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </QuoteState>
+    <AuthState>
+      <QuoteState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </QuoteState>
+    </AuthState>
   );
 };
 
