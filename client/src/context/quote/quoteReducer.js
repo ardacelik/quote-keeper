f1af0a10cec29db711efe47a8714f3bce068f390
Arda Celik <arda.celik@uoit.net>
2019-07-10 22:21:27 -0400

    Get,add,update,delete quotes are added to the reducer

--- client/src/context/quote/quoteReducer.js
+++ client/src/context/quote/quoteReducer.js
@@ -1,31 +1,50 @@
 import {
+  GET_QUOTES,
   ADD_QUOTE,
   DELETE_QUOTE,
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_QUOTE,
   FILTER_QUOTES,
-  CLEAR_FILTER
+  CLEAR_FILTER,
+  QUOTE_ERROR,
+  CLEAR_QUOTES
 } from "../types";
 
 export default (state, action) => {
   switch (action.type) {
+    case GET_QUOTES:
+      return {
+        ...state,
+        quotes: action.payload,
+        loading: false
+      };
     case ADD_QUOTE:
       return {
         ...state,
-        quotes: [...state.quotes, action.payload]
+        quotes: [action.payload, ...state.quotes],
+        loading: false
       };
     case UPDATE_QUOTE:
       return {
         ...state,
         quotes: state.quotes.map(quote =>
-          quote.id === action.payload.id ? action.payload : quote
-        )
+          quote._id === action.payload._id ? action.payload : quote
+        ),
+        loading: false
       };
     case DELETE_QUOTE:
       return {
         ...state,
-        quotes: state.quotes.filter(quote => quote.id !== action.payload)
+        quotes: state.quotes.filter(quote => quote._id !== action.payload)
+      };
+    case CLEAR_QUOTES:
+      return {
+        ...state,
+        quotes: null,
+        filtered: null,
+        error: null,
+        current: null
       };
     case SET_CURRENT:
       return {
@@ -49,6 +68,11 @@ export default (state, action) => {
       return {
         ...state,
         filtered: null
+      };
+    case QUOTE_ERROR:
+      return {
+        ...state,
+        error: action.payload
       };
     default:
       return state;
