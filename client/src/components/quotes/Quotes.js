a09cce79075e2566d14ca7a29cdfdc6b16305cba
Arda Celik <arda.celik@uoit.net>
2019-07-11 21:18:20 -0400

    Added a spinner and quotes are now fetched from the db

--- client/src/components/quotes/Quotes.js
+++ client/src/components/quotes/Quotes.js
@@ -1,33 +1,51 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useEffect } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import QuoteContext from "../../context/quote/quoteContext";
+import Spinner from "../layout/Spinner";
 import QuoteItem from "./QuoteItem";
 
 const Quotes = () => {
   const quoteContext = useContext(QuoteContext);
 
-  const { quotes, filtered } = quoteContext;
+  const { quotes, filtered, getQuotes, loading } = quoteContext;
 
-  if (quotes.length === 0) {
-    return <h4>Please add a quote</h4>;
+  useEffect(() => {
+    getQuotes();
+    // eslint-disable-next-line
+  }, []);
+
+  if (quotes !== null && quotes.length === 0 && !loading) {
+    return <h4 className="m-2">Please add a quote</h4>;
   }
 
   return (
     <div>
       <Fragment>
-        <TransitionGroup>
-          {filtered !== null
-            ? filtered.map(quote => (
-                <CSSTransition key={quote.id} timeout={500} classNames="item">
-                  <QuoteItem quote={quote} />
-                </CSSTransition>
-              ))
-            : quotes.map(quote => (
-                <CSSTransition key={quote.id} timeout={500} classNames="item">
-                  <QuoteItem quote={quote} />
-                </CSSTransition>
-              ))}
-        </TransitionGroup>
+        {quotes !== null && !loading ? (
+          <TransitionGroup>
+            {filtered !== null
+              ? filtered.map(quote => (
+                  <CSSTransition
+                    key={quote._id}
+                    timeout={500}
+                    classNames="item"
+                  >
+                    <QuoteItem quote={quote} />
+                  </CSSTransition>
+                ))
+              : quotes.map(quote => (
+                  <CSSTransition
+                    key={quote._id}
+                    timeout={500}
+                    classNames="item"
+                  >
+                    <QuoteItem quote={quote} />
+                  </CSSTransition>
+                ))}
+          </TransitionGroup>
+        ) : (
+          <Spinner />
+        )}
       </Fragment>
     </div>
   );
