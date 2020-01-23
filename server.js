5426764c8ee3dbc8757ebd852d18e6c14bdc13be
Arda Celik <arda.celik@uoit.net>
2020-01-23 03:29:57 -0500

    Build update

--- server.js
+++ server.js
@@ -9,19 +9,20 @@ connectDB();
 
 // Init middleware
 app.use(express.json({ extended: false }));
+app.use(express.static(path.join(__dirname, "client/build")))
 
 // Define routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/quotes", require("./routes/quotes"));
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+// if(process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   })
+// }
 
 const PORT = process.env.PORT || 5000;
 
